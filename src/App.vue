aa98a70d7eed7765491aa68802cc162c65085a6e
Kevin Chakornsiri <kornmada.p@gmail.com>
2022-05-17 21:22:30 -0400

    Implement routing

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,38 @@
 <script>
 import Navbar from './components/Navbar.vue'
 import Footer from './components/Footer.vue'
+import Home from './components/pages/Home.vue'
+import NotFound from './components/pages/NotFound.vue'
+
+const routes = {
+  '/': Home,
+}
 
 export default {
   components: {
     Navbar,
     Footer
   },
+  data() {
+    return {
+      currentPath: window.location.hash
+    }
+  },
+  computed: {
+    currentView() {
+      return routes[this.currentPath.slice(1) || '/'] || NotFound
+    }
+  },
+  mounted() {
+    window.addEventListener('hashchange', () => {
+		  this.currentPath = window.location.hash
+		})
+  }
 }
 </script>
 
 <template>
-  <Navbar></Navbar>
-
-  <Footer></Footer>
+  <Navbar />
+  <component :is="currentView" />
+  <Footer />
 </template>
