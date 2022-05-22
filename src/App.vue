1a0b4e6323f833015672b2051adeaea7ae58e18a
Kevin Chakornsiri <kornmada.p@gmail.com>
2022-05-22 00:39:17 -0400

    Remove unused import and format eslint

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,38 @@
 <script>
-import Navbar from './components/Navbar.vue'
-import Footer from './components/Footer.vue'
-import Home from './components/pages/Home.vue'
-import Colleagues from './components/pages/Colleagues.vue'
-import Projects from './components/pages/Projects.vue'
-import NotFound from './components/pages/NotFound.vue'
+import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
+import Home from "./components/pages/Home.vue";
+import Colleagues from "./components/pages/Colleagues.vue";
+import Projects from "./components/pages/Projects.vue";
+import NotFound from "./components/pages/NotFound.vue";
 
 const routes = {
-  '/': Home,
-  '/projects': Projects,
-  '/colleagues': Colleagues
-}
+  "/": Home,
+  "/projects": Projects,
+  "/colleagues": Colleagues,
+};
 
 export default {
   components: {
     Navbar,
-    Footer
+    Footer,
   },
   data() {
     return {
-      currentPath: window.location.hash
-    }
+      currentPath: window.location.hash,
+    };
   },
   computed: {
     currentView() {
-      return routes[this.currentPath.slice(1) || '/'] || NotFound
-    }
+      return routes[this.currentPath.slice(1) || "/"] || NotFound;
+    },
   },
   mounted() {
-    window.addEventListener('hashchange', () => {
-		  this.currentPath = window.location.hash
-		})
-  }
-}
+    window.addEventListener("hashchange", () => {
+      this.currentPath = window.location.hash;
+    });
+  },
+};
 </script>
 
 <template>
