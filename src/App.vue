d99099399a832535b5c28259a2711e713e8b95c7
Kevin Chakornsiri <kornmada.p@gmail.com>
2022-05-22 01:27:09 -0400

    Configure routes with vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,21 @@
 <script>
 import Navbar from "./components/Navbar.vue";
 import Footer from "./components/Footer.vue";
-import Home from "./components/pages/Home.vue";
-import Colleagues from "./components/pages/Colleagues.vue";
-import Projects from "./components/pages/Projects.vue";
-import NotFound from "./components/pages/NotFound.vue";
-
-const routes = {
-  "/": Home,
-  "/projects": Projects,
-  "/colleagues": Colleagues,
-};
 
 export default {
   components: {
     Navbar,
     Footer,
   },
-  data() {
-    return {
-      currentPath: window.location.hash,
-    };
-  },
-  computed: {
-    currentView() {
-      return routes[this.currentPath.slice(1) || "/"] || NotFound;
-    },
-  },
-  mounted() {
-    window.addEventListener("hashchange", () => {
-      this.currentPath = window.location.hash;
-    });
-  },
 };
 </script>
 
 <template>
-  <Navbar />
-  <component :is="currentView" />
-  <Footer />
+  <div id="app">
+    <Navbar />
+    <router-view></router-view>
+    <Footer />
+  </div>
 </template>
 
 <style lang="scss">
--- src/main.js
+++ src/main.js
@@ -1,6 +1,28 @@
 import { createApp } from "vue";
+import { createRouter, createWebHashHistory } from "vue-router";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+import Home from "./components/pages/Home.vue";
+import Colleagues from "./components/pages/Colleagues.vue";
+import Projects from "./components/pages/Projects.vue";
+import NotFound from "./components/pages/NotFound.vue";
+
+const routes = [
+  { path: "/", component: Home },
+  { path: "/projects", component: Colleagues },
+  { path: "/colleagues", component: Projects },
+  { path: "/:pathMatch(.*)*", name: "NotFound", component: NotFound },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: routes,
+});
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount("#app");
 
 import "bootstrap/dist/js/bootstrap.js";
